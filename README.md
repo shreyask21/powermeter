9ce7abaac4acb1aaa4dca584009182da149bab43
Shreyas Kulkarni <shreyaskulkarni202@gmail.com>
2020-04-29 23:09:10 +0530

    Switched to ST7789 display

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# powermeter
+# Power Meter
+
+## What is it?
+
+It is a current/voltage sensor based on the texas instruments [INA219 current+voltage sensor](https://www.ti.com/product/INA219) and the [ST7789 TFT display](https://www.adafruit.com/product/3787) combined with powerful [ESP32](https://www.espressif.com/en/products/socs/esp32/overview). It can measure voltage, current and power at the same time.
+
+## Features
+
+1. Captive Portal Webpage
+   When the ESP32 boots up it acts as a wifi access point and serves a captive portal webpage on address `meter.me` or `192.168.1.1`.
+   ![1](https://raw.githubusercontent.com/shreyask21/powermeter/master/img/webpage.png)
+2. AJAX Based Live Graphs
+   The live readings are fetched automatically without refreshing the webpage on a customizable polling rate.
+   ![2](https://raw.githubusercontent.com/shreyask21/powermeter/master/img/graph.gif)
+3. Live Readings on a TFT Display
+
+## Required Libraries/Tools:
+
+1. [Arduino SPIFFS Plugin](https://github.com/me-no-dev/arduino-esp32fs-plugin)
+2. [Adafruit GFX Library](https://github.com/adafruit/Adafruit-GFX-Library)
+3. [Adafruit ST77XX Library](https://github.com/adafruit/Adafruit-ST7735-Library)
+4. [Adafruit INA219 Library](https://github.com/adafruit/Adafruit_INA219)
+5. [ESP32 AsyncTCP Library](https://github.com/me-no-dev/AsyncTCP)
+6. [ESPAsyncWebServer Library](https://github.com/me-no-dev/ESPAsyncWebServer)
--- powermeter.ino
+++ powermeter.ino
@@ -1,128 +1,147 @@
+/**
+    ESP32 Power Meter
+    
+    @author Shreyas Kulkarni
+    @version 0.1 2020/04/29 
+*/
+
+/******************** ESP32 WiFi drivers ********************/
 #include <WiFi.h>
-#include <ESPAsyncWebServer.h>
-#include <DNSServer.h>
 #include <SPIFFS.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_INA219.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
-#include <Fonts/FreeSansBold9pt7b.h>
 
-Adafruit_INA219 ina219;
-Adafruit_SSD1306 display(128, 64, &Wire, 4);
+#include <ESPAsyncWebServer.h>
 AsyncWebServer server(80);
+
+#include <DNSServer.h>
 DNSServer dnsServer;
-const char* ssid = "Power Meter";
-float voltage, current;
+/************************************************************/
+
+/**************** INA219  driver declaration ****************/
+#include <Adafruit_INA219.h>
+#include <Wire.h>
+Adafruit_INA219 INA219;
+/************************************************************/
+
+/**************** ST7789 driver declaration *****************/
+#include <Adafruit_ST7789.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Fonts/FreeSansBold18pt7b.h>
+#define TFT_CS  15
+#define TFT_RST 4
+#define TFT_DC  2
+Adafruit_ST7789 TFT = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+/************************************************************/
+
+/******************   WiFi Configuration   ******************/
+//#define use_password //<-Uncomment to use password for SoftAP
+const char* ssid      =  "Power Meter";
+const char* password  =  "password";
+/************************************************************/
+
 TaskHandle_t Task1;
+float voltage, current;
 
-void readSensorData() {
-  voltage = ina219.getBusVoltage_V() + (ina219.getShuntVoltage_mV() / 1000.0);
-  current = ina219.getCurrent_mA();
-}
-
-void display_data(void * parameter) {
-  while (true) {
-    readSensorData();
-    display.setCursor(0, 12);
-    display.clearDisplay();
-    display.print("V = " + String(voltage)); display.println("V");
-    display.print("I = " + String(current)); display.println("mA");
-    display.print("P = " + String(voltage * current)); display.println("mW");
-    display.display();
-    delay(200);
-  }
-}
-
+/************************** SETUP ***************************/
 void setup() {
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
-  ina219.begin();
-  ina219.setCalibration_32V_2A();
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
-  display.ssd1306_command(SSD1306_SETCONTRAST);
-  display.ssd1306_command(0);
-  display.clearDisplay();
-  display.display();
-  display.setFont(&FreeSansBold9pt7b);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
+  INA219.begin();
+  INA219.setCalibration_16V_400mA();
+  TFT.init(240, 240);
+  TFT.setRotation(2);
+  TFT.fillScreen(ST77XX_BLACK);
+  TFT.setFont(&FreeSansBold18pt7b);
   xTaskCreatePinnedToCore(display_data, "DisplayStask", 10000, NULL, 0, &Task1, 0);
   WiFi.disconnect();
   WiFi.mode(WIFI_OFF);
   WiFi.mode(WIFI_AP);
+#ifdef use_password
+  WiFi.softAP(ssid, password);
+#else
   WiFi.softAP(ssid);
+#endif
   dnsServer.start(53, "*", IPAddress(192, 168, 4, 1));
   SPIFFS.begin();
   server.begin();
   addServerHandlers();
 }
+/************************************************************/
 
+/************************** LOOP ****************************/
+void loop() {
+  dnsServer.processNextRequest();
+}
+/************************************************************/
+
+/****************** Read Data From Sensor *******************/
+void readSensorData() {
+  voltage = INA219.getBusVoltage_V() + (INA219.getShuntVoltage_mV() / 1000.0);
+  current = INA219.getCurrent_mA();
+}
+/************************************************************/
+
+/******************* Display Data To TFT ********************/
+void display_data(void * parameter) {
+  while (true) {
+    readSensorData();
+    TFT.setCursor(0, 25);
+    TFT.print("V = " + String(voltage)); TFT.println("V");
+    TFT.print("I = " + String(current)); TFT.println("mA");
+    TFT.print("P = " + String(voltage * current)); TFT.println("mW");
+    delay(2000);
+  }
+}
+/************************************************************/
+
+/********* Add Listeners For Server HTTP Requests ***********/
 void addServerHandlers() {
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->redirect("http://meter.me/view");
   });
-  
   server.on("/view", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/index.html");
   });
-  
   server.on("/index.html", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/index.html");
   });
-
-  server.on("/datahandler.js", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/datahandler.js", "text/javascript");
-  });
-
   server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/style.css", "text/css");
   });
-
-  server.on("/graph.js", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/graph.js", "text/javascript");
-  });
-
-  server.on("/theme.js", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/theme.js", "text/javascript");
-  });
-
-  server.on("/meter.png", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/meter.png", "image/png");
-  });
-
   server.on("/voltage", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(200, "text/plain", String(voltage).c_str());
   });
-
   server.on("/current", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(200, "text/plain", String(current).c_str());
   });
-
   server.on("/power", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(200, "text/plain", String(voltage * current).c_str());
   });
-
   server.on("/setcalc", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/index.html");
-    ina219.setCalibration_32V_2A();
+    INA219.setCalibration_32V_2A();
   });
-
   server.on("/setcalb", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/index.html");
-    ina219.setCalibration_32V_1A();
+    INA219.setCalibration_32V_1A();
   });
-
   server.on("/setcala", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/index.html");
-    ina219.setCalibration_16V_400mA();
+    INA219.setCalibration_16V_400mA();
   });
-  
+  server.on("/datahandler.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/datahandler.js", "text/javascript");
+  });
+  server.on("/graph.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/graph.js", "text/javascript");
+  });
+  server.on("/theme.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/theme.js", "text/javascript");
+  });
+  server.on("/meter.png", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/meter.png", "image/png");
+  });
   server.on("/git.png", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/git.png");
   });
 }
-
-void loop() {
-  dnsServer.processNextRequest();
-}
+/************************************************************/
